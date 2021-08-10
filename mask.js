a9e3544fb046a0ceca3bd1040f4c919d86d4ffe7
ThiaGoOhhh <88058400+ThiaGoOhhh@users.noreply.github.com>
2021-08-09 21:58:23 -0300

    Update mask.js

--- mask.js
+++ mask.js
@@ -1,19 +1,39 @@
-const inputmask = document.querySelector("#mask")
+const inputMask = document.querySelector("#mask")
 
-const isANumber = (key) => !isNaN(key) && key !="" && key != " "
+const isANumber = (key) => !isNaN(key) && key != "" && key != " "
 
-inputmask.addEventListener("keypress", e=> {
+inputMask.addEventListener("paste", e => e.preventDefault())
+
+inputMask.addEventListener("keydown", e => {
+
+    const deletekeys = ["Backspace", "Delete"]
+    
+    if( !deletekeys.includes(e.key)) return
 
     e.preventDefault()
 
-    if( !isANumber(e.key)) return
+    const onlyNumbers = inputMask.value.replace(".", "").split("")
 
-    const onlyNumbers = inputmask.value.replace(".", "").split("")
+    onlyNumbers.pop()
+    onlyNumbers.splice(0, 0, 0)
+
+    onlyNumbers.splice(1, 0, ".")
+
+    inputMask.value = onlyNumbers.join("")
+
+})
+
+inputMask.addEventListener("keypress", e => {
+   
+    e.preventDefault()
+    if( !isANumber(e.key) ) return
+
+    const onlyNumbers = inputMask.value.replace(".", "").split("")
 
     onlyNumbers.shift()
     onlyNumbers.push(e.key)
 
     onlyNumbers.splice(1, 0, ".")
-    inputmask.value = onlyNumbers.join("")
-}
-)
+
+    inputMask.value = onlyNumbers.join("")
+})
